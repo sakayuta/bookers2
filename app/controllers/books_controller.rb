fd7eefcfb7ce716d30b824335574f63063d133c1
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-06-12 09:08:48 +0000

    [Add bookindex ページ左側]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,15 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    
+    @books = Book.all
+    @user = current_user
   end
   
   def create
     @book = Book.new(book_params)
-    @book.user_id = current.user_id
+    @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
   
   def edit
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  has _one_attaced :image
+  
   belongs_to :user
 end
